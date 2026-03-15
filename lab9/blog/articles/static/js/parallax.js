6ef661df0f4eb163b3ff95423c3d838400190b10
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2026-03-16 02:02:50 +0300

    feat(lab9): init

---
+++ lab9/blog/articles/static/js/parallax.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    console.log("Инициализация параллакс-эффекта...");
+    
+    var yPosition;
+    var scrolled = 0;
+
+    var $parallaxElements = $('.icons-for-parallax img');
+    var $logo = $('.logo');  
+    
+    console.log("Найдено элементов для параллакса:", $parallaxElements.length);
+    
+    $(window).scroll(function() {
+        scrolled = $(window).scrollTop();
+        
+        for (var i = 0; i < $parallaxElements.length; i++) {
+
+            var speed = 0.15 * (i + 1); 
+            yPosition = scrolled * speed;
+            
+            $parallaxElements.eq(i).css({ 
+                top: yPosition,
+                transform: 'rotate(' + (scrolled * 0.02) + 'deg)'
+            });
+        }
+        
+        var logoSpeed = 0.08;
+        var logoYPosition = scrolled * logoSpeed;
+        $logo.css({
+            top: 20 + logoYPosition, 
+            opacity: 1 - (scrolled * 0.001) 
+        });
+        
+        var opacity = Math.min(0.9, scrolled * 0.001);
+        $('.parallax-header').css({
+            'background-color': 'rgba(26, 188, 156, ' + (1 - opacity) + ')'
+        });
+    });
+    
+    $('.icons-for-parallax img').hover(
+        function() {
+            $(this).stop().animate({
+                width: '+=30',
+                opacity: 0.8
+            }, 200);
+        },
+        function() {
+            $(this).stop().animate({
+                width: '-=30',
+                opacity: 1
+            }, 200);
+        }
+    );
+    
+    console.log("Параллакс-эффект активирован!");
+});
